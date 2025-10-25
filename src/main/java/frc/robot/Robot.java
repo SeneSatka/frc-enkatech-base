4d029482a441b52f3c291d0dd77db225b15c8bd7
SeneSatka <senesatka@gmail.com>
2025-10-26 00:18:40 +0300

    feat: AutoChooser added for Autonomous / init and periodic functions added

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,13 +16,24 @@ package frc.robot;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.utils.Checkers;
 
 public class Robot extends LoggedRobot {
+
+  private Command autonomousCommand;
+
+  // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
+
   public Robot() {
     // Set up data receivers & replay source
     // Veri alıcılarının ve yeniden oynatma kaynaklarının belirlenmesi
@@ -52,6 +63,91 @@ public class Robot extends LoggedRobot {
 
     // Check swerve configs - Swerve ayarlarını kontrol eder
     Checkers.checkSwerveConfigures();
+
+    // Auto Chooser for - Otonom için autoChooser
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // Optionally switch the thread to high priority to improve loop
+    // timing (see the template project documentation for details)
+    // Threads.setCurrentThreadPriority(true, 99);
+
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled commands, running already-scheduled commands, removing
+    // finished or interrupted commands, and running subsystem periodic() methods.
+    // This must be called from the robot's periodic block in order for anything in
+
+    CommandScheduler.getInstance().run();
+    // Return to non-RT thread priority (do not modify the first argument)
+    // Threads.setCurrentThreadPriority(false, 10);
+  }
+
+  @Override
+  public void autonomousInit() {
+    // AutonomousCommand get from autoChooser
+    // ShuffleBoard/SmartDashboard daki seçili olan otonom komut alınır.
+
+    autonomousCommand = autoChooser.get();
+
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (autonomousCommand != null) {
+      // Teleopa geçilince otonom komutlarını durdurur.
+      autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {
+  }
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {
+  }
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {
+  }
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
   }
 
 }
