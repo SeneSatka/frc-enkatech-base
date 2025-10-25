ba68b3c467f8e21027cb52ba762cd3fd15493774
SeneSatka <senesatka@gmail.com>
2025-10-26 00:01:51 +0300

    feat: Logger added

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,14 +13,41 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
-/**
- * This class is run automatically. If you change the name of this class or the package after
- * creating this project, you must also update the Main.java file in the project.
- */
 public class Robot extends LoggedRobot {
-  public Robot() {}
+  public Robot() {
+    // Set up data receivers & replay source
+    // Veri alıcılarının ve yeniden oynatma kaynaklarının belirlenmesi
+    switch (Constants.currentMode) {
+      case REAL:
+        // Running on a real robot, log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
 
+      case SIM:
+        // Running a physics simulator, log to NT
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+        // Replaying a log, set up replay source
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    // Start AdvantageKit Logger - AdvantageKit Loglayıcısını başlatır.
+    Logger.start();
+
+  }
 
 }
