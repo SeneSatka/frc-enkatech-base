835e5acb4f2b7ed3f8dbcd4477a9b871eb59b4ad
SeneSatka <senesatka@gmail.com>
2025-10-25 23:48:17 +0300

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,26 @@
+// Copyright 2025-2026 FRC 6985
+// https://www.enkatech6985.com/
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot;
+
+import org.littletonrobotics.junction.LoggedRobot;
+
+/**
+ * This class is run automatically. If you change the name of this class or the package after
+ * creating this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends LoggedRobot {
+  public Robot() {}
+
+
+}
