cd72d937be5d1ef6a0d19c0f3ed9f8494fa6b477
SeneSatka <senesatka@gmail.com>
2025-10-25 23:58:46 +0300

    feat: Constants base and Tuner Constants added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,24 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-import frc.robot.utils.Constants.CanIds;
-import frc.robot.utils.Constants.TunerConstants;
 
 public final class Constants {
     public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
+        /** Running on a real robot. */
+        REAL,
 
-    /** Running a physics simulator. */
-    SIM,
+        /** Running a physics simulator. */
+        SIM,
 
-    /** Replaying from a log file. */
-    REPLAY
-  };
-  public static final Mode simMode = Mode.SIM;
-  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+        /** Replaying from a log file. */
+        REPLAY
+    };
 
-  public static final TunerConstants Tuner = new TunerConstants();
-  public static final CanIds CanId = new CanIds();
-    
+    public static final Mode simMode = Mode.SIM;
+    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
 }
