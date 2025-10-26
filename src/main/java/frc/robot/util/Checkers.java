30247710a219f2c588c82bd85c301f334d94ccae
SeneSatka <senesatka@gmail.com>
2025-10-26 14:41:55 +0300

    refactor: commits edited.

--- src/main/java/frc/robot/util/Checkers.java
+++ src/main/java/frc/robot/util/Checkers.java
@@ -1,3 +1,15 @@
+// Copyright 2025-2026 FRC 6985
+// https://www.enkatech6985.com/
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 package frc.robot.util;
 
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
